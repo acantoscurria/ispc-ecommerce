6d59b9a30684aebf21fd6d20660c2263e58e997f
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-05-07 18:46:22 -0300

    implementando modulos con routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImgComponent } from './components/img/img.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
