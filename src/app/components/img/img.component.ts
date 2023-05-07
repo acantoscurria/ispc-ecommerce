b75957df2735e470fd34a2f3c5147997afd222d9
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-05-07 17:00:56 -0300

    agrregado de proyecto en Angular

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,13 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.css']
+})
+export class ImgComponent {
+ img: string ="valor"
+
+
+
+}
