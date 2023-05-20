fddebaccf4ce04fd0c287f522a615e13f705cc2e
FabiPaez <fitobiopaez@gmail.com>
2023-05-20 16:56:09 -0300

    Aplicando responsive a los productos

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+
+  products: Product[] = [
+    {
+      name:"Pack de latas Quilmes",
+      price: 1800,
+      image: "./assets/images/bulto-lata-quilmes.jpg"
+    },
+    {
+      name:"Pack de latas Schneider",
+      price: 1700,
+      image: "./assets/images/bulto-lata-schneider-x-710cc.jpg"
+    },
+    {
+      name:"Pack de Schweppes",
+      price: 4100,
+      image: "./assets/images/bulto-schweppes.jpg"
+    }
+  ]
+
 }
