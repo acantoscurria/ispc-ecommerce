ad58037f57db2347c753fcce0374f09c16203999
FabiPaez <fitobiopaez@gmail.com>
2023-05-21 15:17:42 -0300

    Agregando estilos a los componentes

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +8,6 @@ import { Product } from './product.model';
 })
 export class AppComponent {
 
-  products: Product[] = [
-    {
-      name:"Pack de latas Quilmes",
-      price: 1800,
-      image: "./assets/images/bulto-lata-quilmes.jpg"
-    },
-    {
-      name:"Pack de latas Schneider",
-      price: 1700,
-      image: "./assets/images/bulto-lata-schneider-x-710cc.jpg"
-    },
-    {
-      name:"Pack de Schweppes",
-      price: 4100,
-      image: "./assets/images/bulto-schweppes.jpg"
-    }
-  ]
+
 
 }
---
+++ my-store/src/app/components/products/products.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from "../../models/product.model";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit{
+
+  products: Product[] = [
+    {
+      id: "1",
+      name:"Pack de latas Quilmes",
+      price: 1800,
+      image: "./assets/images/bulto-lata-quilmes.jpg"
+    },
+    {
+      id: "2",
+      name:"Pack de latas Schneider",
+      price: 1700,
+      image: "./assets/images/bulto-lata-schneider-x-710cc.jpg"
+    },
+    {
+      id:"3",
+      name:"Pack de Schweppes",
+      price: 4100,
+      image: "./assets/images/bulto-schweppes.jpg"
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
