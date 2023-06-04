2c4d783f0837c83ac385819aba71a6b87c9cd64f
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-06-04 09:28:01 -0300

    separando el dashboard

--- Frontend/src/app/layout/layout.module.ts
+++ Frontend/src/app/layout/layout.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { NavDashboardComponent } from './dashboard/nav-dashboard/nav-dashboard.component';
+import { SidebarDashboardComponent } from './dashboard/sidebar-dashboard/sidebar-dashboard.component';
+import { FooterDashboardComponent } from './dashboard/footer-dashboard/footer-dashboard.component';
+
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    NavDashboardComponent,
+    SidebarDashboardComponent,
+    FooterDashboardComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
   ]
 })
 export class LayoutModule { }
