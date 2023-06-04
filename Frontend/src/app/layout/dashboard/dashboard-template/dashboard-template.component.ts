adaac6a1833476856208906fd83934512ea4a8f0
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-06-04 09:33:56 -0300

    separado de dashboard en componentes

---
+++ Frontend/src/app/layout/dashboard/dashboard-template/dashboard-template.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-template',
+  templateUrl: './dashboard-template.component.html',
+  styleUrls: ['./dashboard-template.component.css']
+})
+export class DashboardTemplateComponent {
+
+}
--- Frontend/src/app/layout/layout.module.ts
+++ Frontend/src/app/layout/layout.module.ts
@@ -5,6 +5,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { NavDashboardComponent } from './dashboard/nav-dashboard/nav-dashboard.component';
 import { SidebarDashboardComponent } from './dashboard/sidebar-dashboard/sidebar-dashboard.component';
 import { FooterDashboardComponent } from './dashboard/footer-dashboard/footer-dashboard.component';
+import { DashboardTemplateComponent } from './dashboard/dashboard-template/dashboard-template.component';
 
 
 @NgModule({
@@ -13,12 +14,18 @@ import { FooterDashboardComponent } from './dashboard/footer-dashboard/footer-da
     NavDashboardComponent,
     SidebarDashboardComponent,
     FooterDashboardComponent,
+    DashboardTemplateComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     NavbarComponent,
+    NavDashboardComponent,
+    SidebarDashboardComponent,
+    FooterDashboardComponent,
+    DashboardTemplateComponent,
+
   ]
 })
 export class LayoutModule { }
