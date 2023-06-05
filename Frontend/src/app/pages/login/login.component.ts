42a4672d0b98db8c71786348dddc0fcb04d601aa
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-06-04 22:18:00 -0300

    agregado de conexión con login

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor( private authService: AuthService) {  }
+
+  login(email: string, password: string) {
+    this.authService.login(email, password)
+    .subscribe((data) => {
+      console.log(data.access_token);
+    })
+  }
+
+
 
 }
---
+++ Frontend/src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
+import { Auth } from '../models/auth.models';
+import { TokenService } from './token.service';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = "http://localhost:8000";
+
+  constructor(
+
+    private http: HttpClient,
+    private tokenService: TokenService
+
+    ) { }
+
+  login(email: string, password: string) {
+    return this.http.post<Auth>(this.url + '/auth/login', {email, password}).
+    pipe(
+      tap((data) => {
+        this.tokenService.saveToken(data.access_token);
+
+      })
+    )
+  }
+}
