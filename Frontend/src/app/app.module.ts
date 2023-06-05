d6ae3cb5965e03bf7041d7c9d84763c381a0ca03
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-06-04 22:54:37 -0300

    conexión de login con backend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { TokenInterceptor } from './interceptors/token.interceptor';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +20,8 @@ import { TokenInterceptor } from './interceptors/token.interceptor';
     AppRoutingModule,
     PagesModule,
     HttpClientModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
   ],
   providers: [
     TokenInterceptor
