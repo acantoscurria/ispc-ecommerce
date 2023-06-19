d375ebe719996d654965775ecc178936637be305
AntoFígaro <antobfigaro@gmail.com>
2023-06-19 17:44:11 -0300

    sin cambios

--- Backend/highsystem/TiendavirtualHS/serializers.py
+++ Backend/highsystem/TiendavirtualHS/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
-<<<<<<< HEAD:Backend/entorno/ecommerce/Scripts/highsystem/TiendavirtualHS/serializers.py
-=======
 from .models import Pedido, Carrito, Bebidas, Categoria
 from drf_extra_fields.fields import Base64ImageField
 
->>>>>>> JuanLedesma:Backend/highsystem/TiendavirtualHS/serializers.py
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -20,9 +17,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('email', 'username', 'password')
     def validate_password(self, value):
-<<<<<<< HEAD:Backend/entorno/ecommerce/Scripts/highsystem/TiendavirtualHS/serializers.py
-        return make_password(value)
-=======
         return make_password(value)
 
 class CarritoSerializer(serializers.ModelSerializer):
@@ -49,4 +43,3 @@ class PedidoSerializer(serializers.ModelSerializer):
         model = Pedido
         fields = '__all__'
 
->>>>>>> JuanLedesma:Backend/highsystem/TiendavirtualHS/serializers.py
